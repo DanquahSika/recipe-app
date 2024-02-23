9b82dae1ad60fa5b2919a8c60f8b407cc2428014
DanquahSika <maamesikadanquah203@gmail.com>
2024-02-23 16:09:58 +0000

    This is to start creating a recipe App

---
+++ src/components/navbar/index.jsx
@@ -0,0 +1,17 @@
+
+import {AppBar, Toolbar, Typography} from "@mui/material";
+
+
+const Navbar = () => {
+    return ( 
+        <AppBar position="static">
+        <Toolbar>
+          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+            Recipe App
+          </Typography>
+        </Toolbar>
+      </AppBar>
+    ); 
+}
+ 
+export default Navbar;
--- src/index.js
+++ src/index.js
@@ -4,6 +4,12 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+// the below is font source used by MUI react framework added here because this is always the entry point for import statements after installing the framework to use. You do not have to neccesarily use font source since the alternative is google font CDN  
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
