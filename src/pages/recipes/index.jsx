0e110d3895d50c988df5ae867292f7c112f18bbf
DanquahSika <maamesikadanquah203@gmail.com>
2024-02-26 15:59:37 +0000

    this is to add the recipes from the API

--- src/pages/recipes/index.jsx
+++ src/pages/recipes/index.jsx
@@ -1,26 +1,65 @@
-import { Card, CardContent, CardMedia, Container, Grid, TextField, Typography } from "@mui/material";
+import {
+  Card, CardContent, CardMedia, Container, Grid,
+   TextField, Typography, CardActionArea} from "@mui/material";
+import { useEffect, useState } from "react";
 
 export default function Recipes() {
-    return (
-        <Container sx={{my:'2rem'}} maxWidth="sm">
-            <TextField 
-            fullWidth 
-            id="outlined-basic" 
-            label="Enter a keyword to search and hit enter" 
-            variant="outlined" />
+// to store the recipes after fetching it using API, you go with the useState(a react hook) approach to make sure react keeps it
+    const [recipes,setRecipes] = useState([]); 
 
-            <Grid sx={{mt:'1rem'}} container spacing={3}>
-                <Grid item xs={4}>
-                    <Card>
-                    <CardMedia component="img"
-                    height="100" image="https://images.unsplash.com/photo-1518779578993-ec3579fee39f?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" />
-                    </Card>
-                    <CardContent>
-                        <Typography variant="h5">Recipe Name</Typography>
-                    </CardContent>
-                </Grid>
-            </Grid>
-        </Container>
-    )
+    const getRecipes =() => {
+        // prepare URL
+        const url = new URL("https://api.spoonacular.com/recipes/complexSearch");
+        url.searchParams.append('apiKey', '8a366d5b700243eaab3767afca817585')
+        // fetch recipes from the API
+        // setRecipes will be updated with the recipe state
+        // url.searchParams.append('number', 2) to specify the number of recipes to show
+
+        fetch(url) //this is how fetch works. its default is GET
+        .then(response => response.json())
+        .then(data => {
+             // update recipes state with data
+            setRecipes(data.results);
+        }) //if it fails to do its work the .catch
+        .catch(error =>{
+            console.log(error)
+        });
+       
+    } 
+
+    useEffect(getRecipes, []);
+
+    
+    // Fetch recipe data
+  return (
+    <Container sx={{ my: "2rem" }}>
+      <TextField
+        fullWidth
+        id="outlined-basic"
+        label="Enter a keyword to search and hit enter"
+        variant="outlined"
+      />
+
+      <Grid sx={{ mt: "1rem" }} container spacing={3}>
+        {recipes.map(recipe => (<Grid key={recipe.id} item xs={4}>
+          <Card sx={{ maxWidth: 345, height: "100%"}} >
+            <CardActionArea sx={{height: "100%"}}>
+              <CardMedia
+                component="img"
+                height="140"
+                image= {recipe.image}
+                alt={recipe.title}
+              />
+              <CardContent  sx={{height: "100%"}}>
+                <Typography gutterBottom variant="h5" component="div">
+                  {recipe.title}
+                </Typography>
+               
+              </CardContent>
+            </CardActionArea>
+          </Card>
+        </Grid>))}
+      </Grid>
+    </Container>
+  );
 }
-
